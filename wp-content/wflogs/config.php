7570627deeda9eac21c0e376bbb443892f6284f5
germaneksiiktkhk <german.eksi@gmail.com>
2020-12-11 12:56:50 +0200

    modified file `wflogs`

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1608288724;s:7:"authKey";s:64:"L7ts NqUT|.9$Y$RfL1YyQHY.zWide7u4RaTv<~{$^tbEq7kt?8Jr|fA-j6IsVQZ";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3544;}
