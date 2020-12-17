6c3bf87579edc68c717fecb1c47b245562d97512
germaneksiiktkhk <german.eksi@gmail.com>
2020-12-18 01:42:06 +0200

    modified plugin `my first plugin` version 1.0

--- wp-content/plugins/my-first-plugin/very-first-plugin.php
+++ wp-content/plugins/my-first-plugin/very-first-plugin.php
@@ -8,9 +8,10 @@
  * Authot URI: https://germaneksi.ikt.khk.ee/wordpress/
 **/
 
-// Replaces the excerpt "Read More" text by a link
-function new_excerpt_more($more) {
-       global $post;
-    return '<a class="read-article" href="'. get_permalink($post->ID) . '"> Read the best article ever...</a>';
+function dh_modify_read_more_link() {
+
+    return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+
 }
-add_filter('excerpt_more', 'new_excerpt_more');
+
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
