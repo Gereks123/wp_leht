3ec48bcf1aea2b1a735046a2926750aa48c4d014
germaneksiiktkhk <german.eksi@gmail.com>
2020-12-18 16:21:29 +0200

    modified file `config.php` (after deactivation of `Classic Editor` version 1.6)

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1608288724;s:7:"authKey";s:64:"L7ts NqUT|.9$Y$RfL1YyQHY.zWide7u4RaTv<~{$^tbEq7kt?8Jr|fA-j6IsVQZ";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3544;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"L7ts NqUT|.9$Y$RfL1YyQHY.zWide7u4RaTv<~{$^tbEq7kt?8Jr|fA-j6IsVQZ";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3544;}
